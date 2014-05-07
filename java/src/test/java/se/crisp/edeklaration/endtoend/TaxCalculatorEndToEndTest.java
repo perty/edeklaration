0dd0dee67a1647332e31a2969f865dee481fa71a
Per Lundholm <per@artcomputer.se>
2014-05-07 09:12:31 +0200

    Set some error checking in test to catch configuration problems.

--- java/src/test/java/se/crisp/edeklaration/endtoend/TaxCalculatorEndToEndTest.java
+++ java/src/test/java/se/crisp/edeklaration/endtoend/TaxCalculatorEndToEndTest.java
@@ -5,8 +5,11 @@ import se.crisp.edeklaration.endtoend.util.SimpleOsCommandExecutor;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Simple "end-to-end" test that invokes a java class with two arguments.
@@ -27,23 +30,33 @@ public class TaxCalculatorEndToEndTest {
     @Test
     public void ingenInkomstIngenSkatt() {
         SimpleOsCommandExecutor executor =
-                new SimpleOsCommandExecutor(getJavaExecutablePath(),
-                        Arrays.asList("java", "-cp",
-                                getBuildDir().getAbsolutePath(),
-                                "se.crisp.edeklaration.TaxCalculator", "0", "Stockholms"));
+                new SimpleOsCommandExecutor(getJavaExecutablePath(), getCommand());
         executor.run();
         assertEquals("Slutlig skatt: 0,00 kr", executor.getOutput().get(0));
     }
 
+    private List<String> getCommand() {
+        return Arrays.asList("java", "-cp",
+                getBuildDir().getAbsolutePath(),
+                "se.crisp.edeklaration.TaxCalculator", "0", "Stockholms");
+    }
+
     private String getJavaExecutablePath() {
-        return new File(System.getenv("JAVA_HOME"), "bin").getAbsolutePath();
+        return checkFile(new File(System.getenv("JAVA_HOME"), "bin")).getAbsolutePath();
     }
 
     private File getTestedClass() {
-        return new File(getBuildDir(), "se/crisp/edeklaration/TaxCalculator.class");
+        return checkFile(new File(getBuildDir(), "se/crisp/edeklaration/TaxCalculator.class"));
     }
 
     private File getBuildDir() {
-        return new File(System.getProperty("user.dir"), "build/classes/main");
+        return checkFile(new File(System.getProperty("user.dir"), "build/classes/main"));
+    }
+
+    private File checkFile(File file) {
+        if (!file.exists()) {
+            throw new RuntimeException("Can't find: " + file);
+        }
+        return file;
     }
 }
